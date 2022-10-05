aad9bbff966bd83e473b59ab685c2632c46abae7
n-hirahara <genenao21@gmail.com>
2022-10-05 09:57:10 +0900

    Fix: labels

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -2,20 +2,20 @@ import Head from 'next/head'
 
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import { getAllArticles } from '@/lib/getAllArticles'
 import { formatDate } from '@/lib/formatDate'
+import { getAllArticles } from '@/lib/getAllArticles'
 
 function Article({ article }) {
   return (
-    <article className="md:grid md:grid-cols-4 md:items-baseline">
-      <Card className="md:col-span-3">
+    <article className='md:grid md:grid-cols-4 md:items-baseline'>
+      <Card className='md:col-span-3'>
         <Card.Title href={`/articles/${article.slug}`}>
           {article.title}
         </Card.Title>
         <Card.Eyebrow
-          as="time"
+          as='time'
           dateTime={article.date}
-          className="md:hidden"
+          className='md:hidden'
           decorate
         >
           {formatDate(article.date)}
@@ -24,9 +24,9 @@ function Article({ article }) {
         <Card.Cta>Read article</Card.Cta>
       </Card>
       <Card.Eyebrow
-        as="time"
+        as='time'
         dateTime={article.date}
-        className="mt-1 hidden md:block"
+        className='mt-1 hidden md:block'
       >
         {formatDate(article.date)}
       </Card.Eyebrow>
@@ -38,18 +38,12 @@ export default function ArticlesIndex({ articles }) {
   return (
     <>
       <Head>
-        <title>Articles - Spencer Sharp</title>
-        <meta
-          name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
-        />
+        <title>Articles</title>
+        <meta name='description' content='' />
       </Head>
-      <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
-      >
-        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-          <div className="flex max-w-3xl flex-col space-y-16">
+      <SimpleLayout title='記事一覧' intro=''>
+        <div className='md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40'>
+          <div className='flex max-w-3xl flex-col space-y-16'>
             {articles.map((article) => (
               <Article key={article.slug} article={article} />
             ))}
