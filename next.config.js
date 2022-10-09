fad4b533d6f332a792dc63a48ef0e79ab7856846
n-hirahara <genenao21@gmail.com>
2022-10-09 22:03:15 +0900

    fix: cloud runから取得できるように環境変数を追加

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-// import rehypePrism from '@mapbox/rehype-prism'
-// import nextMDX from '@next/mdx'
-// import remarkGfm from 'remark-gfm'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
@@ -20,13 +15,5 @@ const nextConfig = {
     domains: ['storage.googleapis.com'],
   },
 }
-
-// const withMDX = nextMDX({
-//   extension: /\.mdx?$/,
-//   options: {
-//     remarkPlugins: [remarkGfm],
-//     rehypePlugins: [rehypePrism],
-//   },
-// })
 
 module.exports = nextConfig
